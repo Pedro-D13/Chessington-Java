07403acd7f0b350ae4e42dd3b4f5e0aed5cae0f0
Pedro-D13 <p.afonsosd1994@gmail.com>
2021-09-30 15:13:56 +0100

    Passing tests: red-4 pawns don't move off of the board

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -19,20 +19,22 @@ public class Pawn extends AbstractPiece {
         int moveOneSquare = (this.colour == PlayerColour.WHITE) ? -1 : 1;
         int moveTwoSquares = (this.colour == PlayerColour.WHITE) ? -2 : 2;
         int startSquareForPawn = (this.colour == PlayerColour.WHITE) ? 6 : 1;
-        // move one square
-        Move intendedMove = new Move(from, from.plus(moveOneSquare, 0));
-        // check if there are piece in front
-        if (board.get(intendedMove.getTo()) == null) {
-            movesArray.add(intendedMove);
+        int lessThanZero = from.getRow() - 1;
+        int moreThanEight = from.getRow() + 1;
+        if (lessThanZero >= 0 && moreThanEight <= 7) {
+            // move one square
+            Move intendedMove = new Move(from, from.plus(moveOneSquare, 0));
+            // check if there are piece in front
+            if (board.get(intendedMove.getTo()) == null) {
+                movesArray.add(intendedMove);
+            }
+            // move two squares
+            intendedMove = new Move(from, from.plus(moveTwoSquares, 0));
+            // check if there are piece in front
+            if (startSquareForPawn == from.getRow() && board.get(intendedMove.getTo()) == null) {
+                movesArray.add(intendedMove);
+            }
         }
-        // move two squares
-        intendedMove = new Move(from, from.plus(moveTwoSquares, 0));
-        // check if there are piece in front
-        if (startSquareForPawn == from.getRow() && board.get(intendedMove.getTo()) == null) {
-            movesArray.add(intendedMove);
-        }
-
-
         return movesArray;
     }
 }
