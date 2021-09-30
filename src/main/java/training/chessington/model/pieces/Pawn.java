30e55f1d2b6e4a4c5dccf9f61477bce65a561bc6
Pedro-D13 <p.afonsosd1994@gmail.com>
2021-09-30 17:24:37 +0100

    two failing tests currently

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -19,9 +19,10 @@ public class Pawn extends AbstractPiece {
         int moveOneSquare = (this.colour == PlayerColour.WHITE) ? -1 : 1;
         int moveTwoSquares = (this.colour == PlayerColour.WHITE) ? -2 : 2;
         int startSquareForPawn = (this.colour == PlayerColour.WHITE) ? 6 : 1;
-        int lessThanZero = from.getRow() - 1;
-        int moreThanEight = from.getRow() + 1;
-        if (lessThanZero >= 0 && moreThanEight <= 7) {
+        boolean outOfBoundsRow = outOfBounds(from.getRow() - 1, from.getRow() + 1);
+        boolean outOfBoundsCol = outOfBounds(from.getCol() - 1, from.getCol() + 1);
+        ArrayList<Piece> CanCaptureArr = new ArrayList<>();
+        if (outOfBoundsRow && outOfBoundsCol) {
             // move one square
             Move intendedMove = new Move(from, from.plus(moveOneSquare, 0));
             // check if there are piece in front
@@ -34,7 +35,34 @@ public class Pawn extends AbstractPiece {
             if (startSquareForPawn == from.getRow() && board.get(intendedMove.getTo()) == null) {
                 movesArray.add(intendedMove);
             }
+            // if there is a piece to capture
+            boolean potentialPieceCaptureOne = board.get(from.plus(moveOneSquare, moveOneSquare)) != null;
+            boolean potentialPieceCaptureTwo = board.get(from.plus(moveOneSquare , moveOneSquare  )) != null;
+
+            if (potentialPieceCaptureOne) {
+                if (0 <= from.plus(moveOneSquare, moveOneSquare).getRow()) {
+                    // capture
+                    intendedMove = new Move(from, from.plus(moveOneSquare, moveOneSquare));
+                    movesArray.add(intendedMove);
+                }
+            }
+            if (potentialPieceCaptureTwo) {
+                if (7 >= from.plus(moveOneSquare, moveOneSquare).getRow()) {
+                    // capture
+                    intendedMove = new Move(from, from.plus(moveOneSquare, moveOneSquare));
+                    movesArray.add(intendedMove);
+                }
+            }
+            // if it's not out of bounds
+
+
         }
+
         return movesArray;
     }
+
+    public boolean outOfBounds(int lessThanZero, int moreThanSeven) {
+        return lessThanZero >= 0 && moreThanSeven <= 7;
+    }
+
 }
